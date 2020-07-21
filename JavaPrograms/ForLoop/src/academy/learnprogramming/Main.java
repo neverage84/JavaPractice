e8abf37f0a38b7c97e301068f6e3d3a65d03ca76
Noelle Everage <neverage84@yahoo.com>
2020-07-21 15:03:59 -0700

    Nested for loop

--- JavaPrograms/ForLoop/src/academy/learnprogramming/Main.java
+++ JavaPrograms/ForLoop/src/academy/learnprogramming/Main.java
@@ -1,10 +1,16 @@
 package academy.learnprogramming;
 
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
-	 for (int i = 0; i < 5; i++){
-         System.out.println(i);
+	 for (int i = 9; i >= 0; i--){
+         for (int j = 0; j <= 9; j++){
+             System.out.print(j);
+         }
+         System.out.println();
      }
+
     }
 }
