c9d8f48886e08aa93e7e54894475154001534776
Noelle Everage <neverage84@yahoo.com>
2020-07-21 15:06:52 -0700

    Added formatting

--- JavaPrograms/ForLoop/src/academy/learnprogramming/Main.java
+++ JavaPrograms/ForLoop/src/academy/learnprogramming/Main.java
@@ -5,7 +5,8 @@ import java.sql.SQLOutput;
 public class Main {
 
     public static void main(String[] args) {
-	 for (int i = 9; i >= 0; i--){
+	 for (int i = 0; i <= 9; i++){
+         System.out.printf("row %d ", i);
          for (int j = 0; j <= 9; j++){
              System.out.print(j);
          }
