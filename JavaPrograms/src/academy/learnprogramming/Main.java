57a370032c74fe2344d5ee2304b1cdac11131060
Noelle Everage <neverage84@yahoo.com>
2020-07-21 19:57:39 -0700

    Added if else conditions for rock

---
+++ JavaPrograms/src/academy/learnprogramming/Main.java
@@ -0,0 +1,31 @@
+package academy.learnprogramming;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    private static final int ROCK = 0;
+    private static final int PAPER = 1;
+    private static final int SCISSORS = 2;
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Random randomGenerator = new Random();
+
+        String playerChoice;
+        int playerValue = -1;
+        int computerValue = randomGenerator.nextInt(3);
+
+        System.out.println("Please enter rock, paper or scissors ");
+        playerChoice = scanner.nextLine().toLowerCase();
+
+        if (playerChoice.equals("rock")){
+            playerValue = ROCK;
+        }
+        System.out.printf("Player chose %s, the value is %s%n" , playerChoice, playerValue);
+        scanner.close();
+    }
+}
+
