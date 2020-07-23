0ffe9e371763896d95db1e78fba757dda28f1e52
Noelle Everage <neverage84@yahoo.com>
2020-07-23 16:53:10 -0700

    Operators

---
+++ JavaPrograms/Operators/src/academy/learnprogramming/Main.java
@@ -0,0 +1,10 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int answer = 7 + 3 * 4;
+        System.out.println(answer);
+    }
+}
